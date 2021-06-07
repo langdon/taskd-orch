6c8cffe770f86c4dde92fb85cba79b516138a55f
langdon <langdon@fedoraproject.org>
2021-06-07 17:07:14 -0400

    debugging

--- taskd-container/run.sh
+++ taskd-container/run.sh
@@ -2,5 +2,26 @@
 
 echo "started run.sh"
 echo "taskdata = ${TASKDDATA}"
+
+echo "printing environment"
+env
+
+echo "checking for taskdata"
+if ! test -e ${TASKDDATA}/config; then
+    echo "${TASKDDATA}/config not found"
+fi
+
+array=(one two three four)
+echo ${array[*]}
+
+APPS=( taskd tar rsync )
+echo "checking for apps: ${APPS[*]}"
+for app in "${APPS[@]}"
+do
+    if ! command -v $app &> /dev/null; then
+        echo "$app not found"
+    fi
+done
+
 echo "attempting taskd server --data ${TASKDDATA}"
 exec taskd server --data ${TASKDDATA}
